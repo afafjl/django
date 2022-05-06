1b924789a1e73c9c3516904057828614990dd6c4
afafjl <19522011@gm.uit.edu.vn>
2022-05-06 16:45:58 +0700

    first commit for all files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'store.apps.StoreConfig',
     'taggit',
+    'store.apps.StoreConfig',
+   
 ]
 
 MIDDLEWARE = [
