ce587e328b0353ad17234324ab31ccd697dda937
afafjl <19522011@gm.uit.edu.vn>
2022-05-06 15:49:09 +0700

    first commit for all files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,13 +119,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'static'
-]
+)
+
 MEDIA_URL = 'assets/image/products/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/image/products')
 # Default primary key field type
@@ -134,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/image/products')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 # PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
