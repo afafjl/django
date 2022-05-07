a684c8b11555ef00fadb9f288be9dc784075f926
afafjl <92624368+afafjl@users.noreply.github.com>
2022-05-07 15:23:49 +0700

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include, re_path
 from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
